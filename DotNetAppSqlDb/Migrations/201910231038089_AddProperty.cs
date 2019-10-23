c89324c870119298b00fa88f77403573bcb70a05
Aditya Prakash <adprakas@microsoft.com>
2019-10-23 17:21:39 +0530

    Chess tournament

---
+++ DotNetAppSqlDb/Migrations/201910231038089_AddProperty.cs
@@ -0,0 +1,18 @@
+namespace DotNetAppSqlDb.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddProperty : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Todoes", "Password", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Todoes", "Password");
+        }
+    }
+}
